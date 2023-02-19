e4956cbc7329ba8c76e6e9a89ab83be3111960bf
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2023-02-19 17:09:04 +0900

    [Bronze V] Title: A/B, Time: 224 ms, Memory: 18020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Double A = sc.nextDouble();
+        Double B = sc.nextDouble();
+        System.out.println(A/B);
+    }
+}
