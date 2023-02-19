c406c29a4a09704aea1e727bdd0f1123285a3ff6
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2023-02-20 05:35:58 +0900

    [Silver IV] Title: ATM, Time: 268 ms, Memory: 19976 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        
+        for (int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        
+        int last = 0;
+        int total = 0;
+        for (int i=0; i<n; i++){
+            last += arr[i];
+            total += last;
+        }
+
+        System.out.println(total);
+    }
+}
