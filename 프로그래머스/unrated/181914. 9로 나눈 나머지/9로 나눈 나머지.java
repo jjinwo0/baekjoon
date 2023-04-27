3c2e067798b30e950b88281ac706cbb48f3dece4
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2023-04-27 22:30:54 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.02 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String number){
+        int answer = 0;
+        int temp = 0;
+        for (int i=1; i<=number.length(); i++){
+            temp += Integer.parseInt(number.substring(i-1, i));
+        }
+        answer = temp % 9;
+        
+        return answer;
+    }
+}
