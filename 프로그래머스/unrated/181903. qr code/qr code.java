b5c25212eb956fef047b5fa8510b58f0bfa7bd7e
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2023-04-28 20:41:42 +0900

    [unrated] Title: qr code, Time: 1.27 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181903. qr code/qr code.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int q, int r, String code){
+        String answer = "";
+        for (int i=r+1; i<=code.length(); i+=q){
+            answer += code.substring(i-1, i);
+        }
+
+        return answer;
+    }
+}
