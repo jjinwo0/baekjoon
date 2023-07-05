0d2407c2e11b895cf9d40ae5ea167ce677740bb7
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2023-07-05 17:59:37 +0900

    [Silver V] Title: 셀프 넘버, Time: 160 ms, Memory: 14572 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean[] arr = new boolean[10001];
+
+        for (int i=1; i<10001; i++){
+            int n = check(i);
+            if (n < 10001)
+                arr[n] = true;
+        }
+        for (int i=1; i<arr.length; i++){
+            if (!arr[i])
+                System.out.println(i);
+        }
+    }
+
+    public static int check(int n){
+        int sum = n;
+        while(n != 0){
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
