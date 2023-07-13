b92a5df411d7bf8d11b187d5bb7889406ed33ec1
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2023-07-13 17:57:45 +0900

    [Silver IV] Title: 로프, Time: 992 ms, Memory: 108972 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] rope = new int[N];
+        int answer = 0;
+
+        for (int i=0; i<N; i++){
+            rope[i] = sc.nextInt();
+        }
+        Arrays.sort(rope);
+
+        for (int i=0; i<N; i++){
+            int temp = rope[N - 1 - i] * (i + 1);
+            if (answer < temp)
+                answer = temp;
+        }
+        
+        System.out.println(answer);
+    }
+}
