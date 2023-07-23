4bcf0f094f8934cfc55bb6a199a2fe0798c6a4f1
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2023-07-23 22:41:57 +0900

    [Silver III] Title: 문자열 집합, Time: 416 ms, Memory: 31232 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int answer = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (int i=0; i<N; i++){
+            map.put(br.readLine(), 0);
+        }
+        
+        for (int i=0; i<M; i++){
+            if (map.containsKey(br.readLine()))
+                answer++;
+        }
+
+        System.out.println(answer);
+    }
+}
