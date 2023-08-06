e65e45a4aabe97903f03d55b7be77f424d4c07fb
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2023-08-06 16:10:31 +0900

    [Bronze II] Title: 막대기, Time: 984 ms, Memory: 114776 KB -BaekjoonHub

---
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i=0; i<N; i++){
+            stack.push(sc.nextInt());
+        }
+
+        int count = 1;
+        int last = stack.pop();
+
+        while(!stack.isEmpty()){
+            int next = stack.pop();
+            if (last < next) {
+                count++;
+                last = next;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
