0c494bd25775b9cc4e1c4019565d91b541e0c1cd
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2023-08-16 18:49:54 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -6,11 +6,14 @@ class Solution {
         HashMap<String, Integer> map = new HashMap<>();
         
         for(int i=0; i<10; i++){
+            // 10일까지의 할인 품목 수량 담기
             map.put(discount[i], map.getOrDefault(discount[i], 0) + 1);
         }
         
+        // want 품목이 담겨있는지, 정확한 수량인지 확인하는 메서드
         if(compareWants(map, want, number)) answer++;
         
+        // 10일 내에 모든 물품이 있어야 하므로, 10일 단위로 끊고 반복
         for(int i=0; i<discount.length-10; i++){
             map.replace(discount[i], map.get(discount[i])-1);
             map.put(discount[i+10], map.getOrDefault(discount[i+10], 0)+1);
@@ -24,6 +27,7 @@ class Solution {
     public boolean compareWants(HashMap<String, Integer> map, String[] want, int[] number){
         
         for(int i=0; i<want.length; i++){
+            // 해당 물품이 map에 없거나 map에 담긴 value와 실제 수량이 다른 경우, false 반환
             if(!map.containsKey(want[i]) || map.get(want[i]) != number[i])
                 return false;
         }
