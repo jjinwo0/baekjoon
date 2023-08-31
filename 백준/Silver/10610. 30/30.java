111d3b1d63a2e882297c17ff1d173958e2d4ad2d
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2023-08-31 13:31:21 +0900

    [Silver IV] Title: 30, Time: 408 ms, Memory: 27064 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -1,27 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
+import java.util.Scanner;
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String N = br.readLine();
-        int[] number = new int[N.length()];
-        int addNumbers = 0;
-
-        for (int i=0; i<number.length; i++){
-            number[i] = Integer.parseInt(N.substring(i, i+1));
-            addNumbers += number[i];
+    public static final int MAX = 100000;
+    
+    public static String str;
+    public static int[] numCountArr;
+    public static long strLen;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        str= sc.nextLine();
+        
+        strLen = str.length();
+        numCountArr = new int[10];
+        long total = 0;
+        for(int i=0; i < strLen; i++) {
+            int tNum = Integer.parseInt(str.substring(i, i+1));
+            numCountArr[tNum] += 1;
+            total+=tNum;
         }
-
-        Arrays.sort(number);
-
-        if (addNumbers % 3 == 0 && number[0] == 0){
-            for (int i=number.length-1; i>=0; i--){
-                System.out.print(number[i]);
+        
+        // 0이 없음 == 30의 배수 될 수 없음
+        // 각 자리수의 총 합이 3의 배수가 아니면 종료
+        if(!str.contains("0") || total % 3 != 0) {
+            System.out.println("-1");
+            return;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 9; i >= 0; i--) {
+            while(numCountArr[i] > 0) {
+                sb.append(i);
+                numCountArr[i]--;
             }
-        }else
-            System.out.println(-1);
+        }
+        System.out.println(sb.toString());
     }
 }
