6a57f6702262d45105cb86fe2e99dbd8e01cb9e1
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2023-09-15 21:47:14 +0900

    [level 2] Title: 짝지어 제거하기, Time: 49.65 ms, Memory: 59 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+class Solution
+{
+
+    public int solution(String s) {
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+
+        for (int i=0; i<s.length(); i++){
+            if (!stack.isEmpty() && stack.peek() == s.charAt(i))
+                stack.pop();
+            else{
+                stack.push(s.charAt(i));
+            }
+        }
+        if (stack.isEmpty())
+            answer = 1;
+
+        return answer;
+    }
+}
