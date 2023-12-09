f6314b41fb2f4d7838bb821961a366f2cc2b17c2
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2023-12-09 20:08:14 +0900

    [Silver III] Title: 칸토어 집합, Time: 328 ms, Memory: 35740 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+
+        while((str = br.readLine()) != null) {
+
+            int N = Integer.parseInt(str);
+
+            if (N == 0)
+                System.out.println("-");
+            else System.out.println(cantor(N));
+        }
+    }
+
+    public static String cantor(int n){
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        if (n == 0){
+            return "-";
+        }
+
+        for (int i=0; i<Math.pow(3, n-1); i++){
+            sb1.append(" ");
+        }
+        String answer = sb2.append(cantor(n - 1)).append(sb1).append(cantor(n - 1)).toString();
+
+        return answer;
+    }
+}
