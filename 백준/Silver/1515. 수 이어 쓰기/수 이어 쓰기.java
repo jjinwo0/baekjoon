b96aaa5db4460260a07349dce2758e3725e217f3
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2023-12-26 16:42:52 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 156 ms, Memory: 16184 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        int current = 0;
+        int pick = 0;
+
+        while (pick++ <= Integer.MAX_VALUE) {
+            String temp = String.valueOf(pick);
+
+            for (int i = 0; i < temp.length(); i++) {
+                if (temp.charAt(i) == str.charAt(current))
+                    current++;
+
+                if (current == str.length()) {
+                    System.out.println(pick);
+                    return ;
+                }
+            }
+        }
+    }
+}
