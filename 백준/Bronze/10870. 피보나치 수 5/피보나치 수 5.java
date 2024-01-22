1f1c95579e47a1d3a3f320a85be93a257a4c684f
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2024-01-22 13:32:24 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 208 ms, Memory: 17664 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -1,25 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        if (n == 0){
-            System.out.println(0);
-        }
-        else if (n == 1){
-            System.out.println(1);
-        }
-        else {
-            int[] arr = new int[n+1];
-            arr[0] = 0;
-            arr[1] = 1;
-            for (int i = 2; i <= n; i++) {
-                arr[i] = arr[i - 2] + arr[i - 1];
-            }
-            System.out.println(arr[n]);
-        }
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(fibo(n));
+	}
+	
+	static int fibo(int n) {
+		if(n == 0) {
+			return 0;
+		}
+		else if(n == 1){
+			return 1;
+		}
+		else {
+			return fibo(n - 1) + fibo(n - 2);
+		}
+	}
 }
