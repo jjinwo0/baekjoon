b04f78812ab2c309d2f5e85b985b62439d76e7b2
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2024-01-27 22:00:38 +0900

    [Silver III] Title: N과 M (7), Time: 780 ms, Memory: 264840 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+
+    static int[] arr, temp;
+
+    static boolean[] visited;
+
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        temp = new int[M];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        recursion(0, 0);
+
+        System.out.println(sb.toString());
+    }
+
+    private static void recursion(int depth, int start) {
+
+        if (depth == M){
+            for (int i=0; i<temp.length; i++){
+                sb.append(temp[i] + " ");
+            }
+            sb.append("\n");
+            return ;
+        }
+
+        for (int i=start; i<N; i++){
+            visited[i] = true;
+            temp[depth] = arr[i];
+            recursion(depth+1, start);
+            visited[i] = false;
+        }
+    }
+}
