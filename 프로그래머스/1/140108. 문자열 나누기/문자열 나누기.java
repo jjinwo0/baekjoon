e75472f51468e893a8c840052bde3f084542673c
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2024-01-31 00:35:51 +0900

    [level 1] Title: 문자열 나누기, Time: 6.63 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,40 +1,6 @@
 class Solution {
-
-    static int count = 0;
-    
-    public static int solution(String str){
-        
-        substr(str);
-        
-        return count;
-    }
-
-    private static void substr(String str) {
-
-
-        if (str.length() == 0)
-            return;
-
-        char first = str.charAt(0);
-        
-        int sameCount = 1;
-        int otherCount = 0;
-        for (int i=1; i<str.length(); i++){
-
-            if (str.charAt(i) == first)
-                sameCount++;
-
-            else{
-                otherCount++;
-            }
-
-            if (sameCount == otherCount){
-
-                String substring = str.substring(i + 1, str.length());
-                substr(substring);
-                break;
-            }
-        }
-        count++;
+    public int solution(String s) {
+        int answer = 0;
+        return answer;
     }
 }
