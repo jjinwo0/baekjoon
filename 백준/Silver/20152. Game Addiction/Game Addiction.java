045df1261be72202baddb58df6a22ad14aa37bc9
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2024-02-05 22:01:03 +0900

    [Silver II] Title: Game Addiction, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/20152. Game Addiction/Game Addiction.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int H = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int idx = Math.abs(H - N);
+        long[][] map = new long[idx+1][idx+1];
+
+        for (int i=0; i<=idx; i++){
+            for (int j=0; j<=idx; j++){
+                if (j < i){
+                    map[i][j] = 0;
+                }else
+                    map[i][j] = 1;
+            }
+        }
+
+        for (int i=1; i<=idx; i++){
+
+            for (int j=1; j<=idx; j++){
+                if (j < i) continue;
+                map[i][j] = map[i][j-1] + map[i-1][j];
+            }
+        }
+
+        System.out.println(map[idx][idx]);
+
+    }
+}
