4bd314ed7e1766f62fc493281eb07b1bfc6601bc
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2024-02-07 14:48:47 +0900

    [Gold III] Title: 정상 회담 2, Time: 164 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Gold/1670. 정상 회담 2/정상 회담 2.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[N+1];
+
+        dp[0] = 1;
+        dp[2] = 1;
+
+        for (int i=4; i<=N; i+=2){
+            for (int j=0; j<=i-2; j+=2){
+                dp[i] += dp[i-j-2] * dp[j];
+                dp[i] %= 987654321;
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
