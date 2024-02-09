dd359ad6d9c773433c54b12123c33715e74ab502
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2024-02-10 03:59:41 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.15 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+class Solution {
+    public int solution(int[] ingredient){
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i : ingredient){
+            stack.push(i);
+
+            if (stack.size() >= 4){
+                if (stack.get(stack.size()-4) == 1
+                && stack.get(stack.size()-3) == 2
+                && stack.get(stack.size()-2) == 3
+                && stack.get(stack.size()-1) == 1) {
+                    answer++;
+                    for (int cnt = 0; cnt < 4; cnt++) {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
