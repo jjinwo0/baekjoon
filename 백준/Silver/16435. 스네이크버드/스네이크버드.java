92b1f89b6b59e78a59698cd5f365e2a0700cec11
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2024-02-13 14:02:46 +0900

    [Silver V] Title: 스네이크버드, Time: 136 ms, Memory: 14584 KB -BaekjoonHub

--- 백준/Silver/16435. 스네이크버드/스네이크버드.java
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -1,23 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
 
-		int N = sc.nextInt();
-		int snake = sc.nextInt();
+        Integer[] arr = new Integer[N];
 
-		int[] arr = new int[N];
-		for (int i = 0; i < arr.length; i++)
-			arr[i] = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-		Arrays.sort(arr);
-		for (int i = 0; i < arr.length; i++) {
-			if (snake >= arr[i])
-				++snake;
-		}
-		System.out.println(snake);
-	}
+        Arrays.sort(arr, (o1, o2) -> o1-o2);
+
+        for (int i=0; i<N; i++){
+            if (arr[i] <= L){
+                L++;
+            } else break;
+        }
+
+        System.out.println(L);
+    }
 }
