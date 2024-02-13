ba3d97ba632244682b4e2fc820f9a1ad281d865f
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2024-02-13 13:21:41 +0900

    [Silver III] Title: 다음 순열, Time: 172 ms, Memory: 15372 KB -BaekjoonHub

--- 백준/Silver/10972. 다음 순열/다음 순열.java
+++ 백준/Silver/10972. 다음 순열/다음 순열.java
@@ -9,6 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
         num = new int[N];
@@ -18,32 +19,25 @@ public class Main {
             num[i] = Integer.parseInt(st.nextToken());
         }
 
-        if(check()) {
-            for (int i=0; i<num.length; i++){
-                System.out.print(num[i] +  " ");
-            }
-        }
-
-        else System.out.println(-1);
-    }
-
-    private static boolean check() {
-
-        int temp = num.length - 1;
-
-        while(temp > 0 && num[temp-1] >= num[temp])
+        int temp = num.length-1;
+        while(temp > 0 && num[temp-1] > num[temp])
             temp--;
 
-        if (temp <= 0)
-            return false;
+        if (temp <= 0) {
+            sb.append(-1);
+            System.out.println(sb);
+            return;
+        }
 
-        int temp2 = num.length - 1;
-
-        while(num[temp-1] >= num[temp2])
+        int temp2 = num.length-1;
+        while(num[temp2] <= num[temp-1])
             temp2--;
 
-        if (temp2 <= 0)
-            return false;
+        if (temp2 <= 0) {
+            sb.append(-1);
+            System.out.println(sb);
+            return;
+        }
 
         swap(temp-1, temp2);
 
@@ -55,9 +49,12 @@ public class Main {
             temp+=1;
         }
 
-        return true;
-    }
+        for (int i=0; i<num.length; i++){
+            sb.append(num[i]).append(" ");
+        }
 
+        System.out.println(sb);
+    }
     private static void swap(int i, int j) {
 
         int temp = num[i];
