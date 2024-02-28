48d1d2e093ed30ffb883873f7ba293d34ecd131b
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2024-02-29 00:31:45 +0900

    [Silver I] Title: 곱셈, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        long answer = mult(A%C, B, C);
+
+        System.out.println(answer);
+    }
+
+    private static long mult(long A, long B, long C) {
+
+        if (B == 1){
+            return A % C;
+        }
+
+        long temp = mult(A, B / 2, C) % C;
+
+        if (B % 2 == 1)
+            return (A * ((temp * temp) % C)) % C;
+
+        return temp * temp % C;
+    }
+}
