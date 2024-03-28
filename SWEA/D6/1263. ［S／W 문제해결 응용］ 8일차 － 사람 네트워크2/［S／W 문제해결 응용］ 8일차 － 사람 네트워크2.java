ca22d42cc9fb33fe19dc98cccb03ca4bdd736b41
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2024-03-28 17:00:45 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 1,234 ms, Memory: 107,168 KB -BaekjoonHub

--- SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -1,48 +1,55 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int t=1;t<=T;t++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			int[][] board = new int[n][n];
-			for(int i=0;i<n;i++) {
-				for(int j=0;j<n;j++) {
-					board[i][j]=Integer.parseInt(st.nextToken());
-					if(board[i][j]==0) {
-						board[i][j]=1000000000;
-					}
-				}
-			}
-			for(int k=0;k<n;k++) {
-				for(int i =0; i<n;i++) {
-					if(k==i) continue;
-					for(int j=0;j<n;j++) {
-						if(j==k||i==j) continue;
-						board[i][j]= Math.min(board[i][k]+board[k][j],board[i][j]);
-					}
-				}
-			}
-			int answer = Integer.MAX_VALUE;
-			for(int i=0;i<n;i++) {
-				int temp=0;
-				for(int j=0;j<n;j++) {
-					if(i==j) continue;
-					temp+=board[i][j];
-				}
-				answer = Math.min(answer, temp);
-			}
-			System.out.printf("#%d %d\n",t,answer);
-		}
-		
-		
-		
-	}
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc=1; tc<=T; tc++){
+            sb.append("#").append(tc).append(" ");
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int N = Integer.parseInt(st.nextToken());
+
+            int[][] dist = new int[N][N];
+
+            for (int i=0; i<N; i++){
+                for (int j=0; j<N; j++){
+
+                    dist[i][j] = Integer.parseInt(st.nextToken());
+
+                    if (i != j && dist[i][j] == 0) dist[i][j] = 100_000_000;
+                }
+            }
+
+            for (int k=0; k<N; k++){
+
+                for (int i=0; i<N; i++){
+                    for (int j=0; j<N; j++){
+                        dist[i][j] = Math.min(dist[i][j], dist[k][j] + dist[i][k]);
+                    }
+                }
+            }
+
+            int answer = Integer.MAX_VALUE;
+
+            for (int i=0; i<N; i++){
+                int temp = 0;
+
+                for (int j=0; j<N; j++){
+                    temp += dist[i][j];
+                }
+                answer = Math.min(answer, temp);
+            }
+
+            sb.append(answer).append("\n");
+        }
+
+        System.out.println(sb);
+    }
 }
