d92459c322609995c9a3e239726c040978ce143f
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2024-03-29 15:14:19 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 278 ms, Memory: 97,740 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -1,147 +1,164 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Solution {
+/**
+ * 
+ * 
+ * 
+ * 접근 방법 : 완전 탐색으로 접근시
+ * 모든 경우는 W^N 이며, 각각의 경우에 공을 부수는 최악의 경우는 W*H, 공을 내려가게 만드는 최악의 경우 또한 W*H
+ * 고로 W^N개의 경우에 대해 각각 2*N*W*H의 시간이 걸린다.
+ * 계산시 약 2800만으로 시간안에 해결가능함을 알 수 있다.
+ * 
+ * @author 박범수
+ *
+ */
+public class Solution{
 
-    static int N, W, H, answer;
+	static class node {
+		int y;
+		int x;
+		int power;
 
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
+		public node(int y, int x, int power) {
+			this.y = y;
+			this.x = x;
+			this.power = power;
+		}
+	}
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
+		int test_cases = Integer.parseInt(br.readLine());
 
-        for (int tc=1; tc<=T; tc++){
+		for (int test_case = 1; test_case <= test_cases; test_case++) {
+			// 입력
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int W = Integer.parseInt(st.nextToken());
+			int H = Integer.parseInt(st.nextToken());
+			int[][] square = new int[H][W];
+			for (int i = 0; i < H; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < W; j++) {
+					square[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
 
-            sb.append("#").append(tc).append(" ");
+			// W^N 의 경우의 수
+			int answer = dropballs(square, N, W, H, 0);
+			System.out.printf("#%d %d\n",test_case,answer);
+		}
 
-            StringTokenizer st = new StringTokenizer(br.readLine());
+	}
 
-            N = Integer.parseInt(st.nextToken());
-            W = Integer.parseInt(st.nextToken());
-            H = Integer.parseInt(st.nextToken());
-            answer = Integer.MAX_VALUE;
+	private static int dropballs(int[][] square, int n, int w, int h, int depth) {
+		if (depth == n) {
+			return count_bricks(square,h,w);
+		}
+		//
+		int answer = Integer.MAX_VALUE;
+		for (int i = 0; i < w; i++) {
+			int[][] copy = copysquare(square, h, w);
+			// 공 깨기
+			breakbricks(copy, h, w, i);
+			// 공 내리기
+			downbricks(copy,h,w);
+			answer = Math.min(answer, dropballs(copy, n, w, h, depth + 1));
+			//System.out.println("answer :"+ depth + " "+ answer);
+		}
+//		int[][] copy = copysquare(square, h, w);
+//		// 공 깨기
+//		breakbricks(copy, h, w, 2);
+//		// 공 내리기
+//		downbricks(copy,h,w);
+//		answer = Math.min(answer, dropballs(copy, n, w, h, depth + 1));
+//		System.out.println("answer :"+ depth + " "+ answer);
+//		for (int i = 0; i < h; i++) {
+//			for (int j = 0; j < w; j++) {
+//				System.out.print(square[i][j]);
+//			}
+//			System.out.println();
+//		}
+		return answer;
+	}
 
-            int[][] map = new int[H][W];
+	private static void downbricks(int[][] copy, int h, int w) {
+		for(int i=0;i<w;i++) {
+			int count =h-1;
+			for(int j=h-1;j>=0;j--) {
+				if(copy[j][i]!=0) {
+					int temp = copy[j][i];
+					copy[j][i]=0;
+					copy[count--][i]=temp;
+					
+				}
+			}
+		}
+	}
 
-            for (int i=0; i<H; i++){
-                st = new StringTokenizer(br.readLine());
-                for (int j=0; j<W; j++){
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
+	private static void breakbricks(int[][] copy, int h, int w, int position) {
+		// 초기 위치 찾기
+		node start = null;
+		for (int i = 0; i < h; i++) {
+			if (copy[i][position] != 0) {
+				start = new node(i, position, copy[i][position]);
+				break;
+			}
+		}
+		if (start == null)
+			return;
+		int[] dy = { 0, 0, -1, 1 };
+		int[] dx = { -1, 1, 0, 0 };
+		Queue<node> queue = new ArrayDeque<>();
+		queue.add(start);
+		copy[start.y][start.x]=0;
+		//System.out.println(start.y + " " + start.x + " " + start.power);
+		while (!queue.isEmpty()) {
+			node now = queue.poll();
+			int y = now.y;
+			int x = now.x;
+			int power = now.power;
+			//System.out.println("근원 : "+ y + " "+ x + " " + power);
+			for (int i = 0; i < 4; i++) {
+				for (int j = 1; j <= power; j++) {
+					int nexty = y + (dy[i] * (j-1));
+					int nextx = x + (dx[i] * (j-1));
 
-            if (blockCount(map) == 0) {
-                sb.append(0).append("\n");
-                continue;
-            }
+					if (nexty < 0 || nexty >= h || nextx < 0 || nextx >= w)
+						break;
 
-            breakBlock(0, map);
+					if (copy[nexty][nextx] == 0)
+						continue;
 
-            if (answer == Integer.MAX_VALUE)
-                sb.append(0).append("\n");
+					queue.add(new node(nexty,nextx,copy[nexty][nextx]));
+					//System.out.println("터짐 : "+ nexty + " "+ nextx + " " + copy[nexty][nextx]);
+					copy[nexty][nextx] = 0;
+				}
+			}
+			//System.out.println("------copy[nexty][nextx]");
+		}
+	}
 
-            else sb.append(answer).append("\n");
-        }
+	private static int[][] copysquare(int[][] square, int h, int w) {
+		int[][] copy = new int[h][w];
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				copy[i][j] = square[i][j];
+			}
+		}
 
-        System.out.println(sb);
-    }
+		return copy;
+	}
 
-    private static int blockCount(int[][] map) {
-
-        int count = 0;
-
-        for (int i=0; i<H; i++){
-            for (int j=0; j<W; j++){
-                if (map[i][j] != 0)
-                    count++;
-            }
-        }
-
-        return count;
-    }
-
-    private static void breakBlock(int n, int[][] map) {
-
-        if (n == N) {
-            answer = Math.min(answer, blockCount(map));
-            return;
-        }
-
-        for (int j=0; j<W; j++){
-
-            int[][] chMap = copy(map);
-
-            for (int i=0; i<H; i++){
-                if (chMap[i][j] != 0){
-
-                    dfs(i, j, chMap);
-                    dropBlocks(chMap);
-                    breakBlock(n+1, chMap);
-
-                    break;
-                }
-            }
-        }
-    }
-
-    private static void dropBlocks(int[][] map) {
-        for (int j = 0; j < W; j++) {
-            for (int i = H - 1; i >= 0; i--) {
-
-                if (map[i][j] == 0) {
-
-                    int up = i - 1;
-
-                    while (up >= 0 && map[up][j] == 0)
-                        up--;
-
-                    if (up >= 0) {
-                        map[i][j] = map[up][j];
-                        map[up][j] = 0;
-                    }
-                }
-            }
-        }
-    }
-
-
-    private static int[][] copy(int[][] map) {
-        if (map == null)
-            return null;
-
-        int[][] result = new int[map.length][];
-
-        for (int i = 0; i < map.length; i++)
-            result[i] = Arrays.copyOf(map[i], map[i].length);
-
-        return result;
-    }
-
-    private static void dfs(int i, int j, int[][] map) {
-
-        if (map[i][j] == 0)
-            return;
-
-        int value = map[i][j];
-
-        map[i][j] = 0;
-
-        for (int k = 0; k < 4; k++) {
-            for (int v=1; v<value; v++) {
-                int ny = i + dy[k] * v;
-                int nx = j + dx[k] * v;
-
-                if (ny >= 0 && nx >= 0 && ny < H && nx < W && map[ny][nx] > 0) {
-                    dfs(ny, nx, map);
-                }
-            }
-        }
-    }
+	private static int count_bricks(int[][] square, int h, int w) {
+		int answer =0;
+		for(int i=0;i<w;i++) {
+			for(int j=0;j<h;j++) {
+				if(square[j][i]!=0)answer++;
+			}
+		}
+		return answer;
+	}
 }
