bc77138a17f66bbe2a407f9bb10bd6ef49b861fa
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2024-04-02 13:42:13 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 124 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -1,21 +1,36 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+/**
+ * [B2] 백준 10870 피보나치수5
+ * 메모리 : 14192KB
+ * 시간 : 124ms
+ * 코드 길이 : 569B
+ * 
+ * @author 박범수
+ * @see <a href="https://www.acmicpc.net/problem/10870">
+ */
 
-public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		System.out.println(fibo(n));
-	}
-	
-	static int fibo(int n) {
-		if(n == 0) {
-			return 0;
-		}
-		else if(n == 1){
-			return 1;
-		}
-		else {
-			return fibo(n - 1) + fibo(n - 2);
-		}
-	}
+public class Main 
+{	
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+    	// 입력 받기
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(bf.readLine());
+        System.out.println(fibo(a));
+    	
+    }
+    
+    public static int fibo(int n) {
+    	if(n==0) {
+    		return 0;
+    	}
+    	if(n==1) {
+    		return 1;
+    	}
+    	return fibo(n-1)+fibo(n-2);
+    }
 }
+
+
+
