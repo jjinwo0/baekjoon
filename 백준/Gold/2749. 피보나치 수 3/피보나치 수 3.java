df9adeedfc3f08a27677278484af102777784318
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2024-04-10 17:05:18 +0900

    [Gold II] Title: 피보나치 수 3, Time: 104 ms, Memory: 23272 KB -BaekjoonHub

--- 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
@@ -1,30 +1,23 @@
-import java.util.*;
-import java.io.*;
-public class Main{
-	static final int p =1000000;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		long n = Long.parseLong(br.readLine());
-		System.out.println(findfibo(n)[1][0]);
-	}
-	private static long[][] findfibo(long n) {
-		if(n==1) return new long[][] {{1,1},{1,0}};
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		long[][] matrix = findfibo(n/2);
-		if(n%2==0) {
-			return doMatrix(matrix,matrix);
-		}else{
-			return doMatrix(doMatrix(matrix,matrix),findfibo(1));
+		int pisano = 1500000;
+		long size = Long.parseLong(br.readLine()) % pisano;
+		
+		long[] num = new long[pisano + 1];
+		
+		num[0] = 0;
+		num[1] = 1;
+		
+		for(int i = 2; i <= pisano; i++) {
+			num[i] = (num[i - 1] + num[i - 2]) % 1000000;
 		}
-	}
-	private static long[][] doMatrix(long[][] a , long[][] b){
-		long[][] answer = new long[2][2];
 		
-		answer[0][0]= ((a[0][0]*b[0][0])%p+ (a[0][1]*b[1][0])%p)%p;
-		answer[0][1]= ((a[0][0]*b[0][1])%p+ (a[0][1]*b[1][1])%p)%p;
-		answer[1][0]= ((a[1][0]*b[0][0])%p+ (a[1][1]*b[1][0])%p)%p;
-		answer[1][1]= ((a[1][0]*b[0][1])%p+ (a[1][1]*b[1][1])%p)%p;
-		
-		return answer;
+		System.out.print(num[(int)size]);
 	}
 }
