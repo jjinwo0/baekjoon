5eb8b401c4a2f901bd7036c563afbb5e42d1e12d
Jinwoo Park <86451515+jjinwo0@users.noreply.github.com>
2024-04-15 10:19:07 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 100 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -1,79 +1,95 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main{
-	static class node {
-		int y;
-		int x;
-		int count;
-		int key;
+public class Main {
 
-		public node(int y, int x, int count, int key) {
-			this.y = y;
-			this.x = x;
-			this.count = count;
-			this.key = key;
-		}
-	}
+    static class Minsik {
+        int y;
+        int x;
+        int dist;
+        int keys;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		char[][] board = new char[n][m];
-		node start = null;
-		for (int i = 0; i < n; i++) {
-			char[] list = br.readLine().toCharArray();
-			for (int j = 0; j < m; j++) {
-				board[i][j] = list[j];
-				if (board[i][j] == '0') {
-					start = new node(i, j, 0, 0);
-				}
-			}
+        public Minsik(int y, int x, int dist, int keys) {
+            this.y = y;
+            this.x = x;
+            this.dist = dist;
+            this.keys = keys;
+        }
+    }
 
-		}
-		System.out.println(bfs(start, board, n, m));
+    static int N, M, answer = -1;
+    static int[] dX = {1, 0, -1, 0};
+    static int[] dY = {0, 1, 0, -1};
+    static char[][] map;
+    static boolean[][][] visited;
 
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	private static int bfs(node start, char[][] board, int n, int m) {
-		int[] dy = { 0, 0, -1, 1 };
-		int[] dx = { -1, 1, 0, 0 };
-		int answer = -1;
-		boolean[][][] visited = new boolean[(1<<6)][n][m];
-		Queue<node> queue = new ArrayDeque<>();
-		queue.add(start);
-		visited[0][start.y][start.x]=true;
-		while (!queue.isEmpty()) {
-			node current = queue.poll();
-			int y = current.y;
-			int x = current.x;
-			int count = current.count;
-			//System.out.println(y+" "+x+" "+count+" "+current.key);
-			if(board[y][x]=='1') {
-				return count;
-			}
-			for (int i = 0; i < 4; i++) {
-				int ny = y + dy[i];
-				int nx = x + dx[i];
-				int key = current.key;
-				if (ny < 0 || ny >= n || nx < 0 || nx >= m || board[ny][nx]=='#')continue;
-				int num = board[ny][nx] - 'a';
-				int num2 = board[ny][nx] - 'A';
-				
-				if (num >= 0 && num < 6) key = ( key | (1 << num) );
-				
-				if (num2 >= 0 && num2 < 6 ) {
-					if((key&(1<<num2))<=0) continue;
-				}
-				if (visited[key][ny][nx]) continue;
-				visited[key][ny][nx] = true;
-				queue.add(new node(ny,nx,count+1,key));
-			}
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-		}
-		return -1;
-	}
+        map = new char[N][M];
+        visited = new boolean[N][M][1 << 6];
 
+        Minsik minsik = null;
+
+        for (int i=0; i<N; i++){
+            map[i] = br.readLine().toCharArray();
+            for (int j=0; j<M; j++){
+                if (map[i][j] == '0')
+                    minsik = new Minsik(i, j, 0, 0);
+            }
+        }
+
+        bfs(minsik);
+
+        System.out.println(answer);
+    }
+
+    private static void bfs(Minsik minsik) {
+        Queue<Minsik> queue = new LinkedList<>();
+        queue.add(minsik);
+        visited[minsik.y][minsik.x][minsik.keys] = true;
+
+        while (!queue.isEmpty()) {
+            Minsik now = queue.poll();
+
+            if (map[now.y][now.x] == '1') {
+                answer = now.dist;
+                return;
+            }
+
+            for (int i=0; i<4; i++){
+                int ny = now.y + dY[i];
+                int nx = now.x + dX[i];
+
+                if (ny >= 0 && nx >= 0 && ny < N && nx < M && !visited[ny][nx][now.keys]) {
+                    char loc = map[ny][nx];
+
+                    if (loc == '#') continue;
+
+                    if (loc >= 'a' && loc <= 'f') {
+                        int newKeys = (now.keys | (1 << (loc - 'a')));
+                        visited[ny][nx][newKeys] = true;
+                        queue.add(new Minsik(ny, nx, now.dist + 1, newKeys));
+                    } else if (loc >= 'A' && loc <= 'F') {
+                        if ((now.keys & (1 << (loc - 'A'))) != 0) {
+                            visited[ny][nx][now.keys] = true;
+                            queue.add(new Minsik(ny, nx, now.dist + 1, now.keys));
+                        }
+                    } else {
+                        visited[ny][nx][now.keys] = true;
+                        queue.add(new Minsik(ny, nx, now.dist + 1, now.keys));
+                    }
+                }
+            }
+        }
+        answer = -1;
+    }
 }
