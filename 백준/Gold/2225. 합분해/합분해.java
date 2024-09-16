7cb2eb68ac3192852777349c2e81b0c417c2b5cc
Jinwoo Park <zvzv9808@gmail.com>
2024-09-16 23:27:40 +0900

    [Gold V] Title: 합분해, Time: 64 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -13,20 +13,19 @@ public class Main {
         int K = Integer.parseInt(st.nextToken());
         final int MOD = 1000000000;
 
-        int[][] dp = new int[N + 1][K + 1];
+        int[][] dp = new int[N+1][K+1];
 
-        // Initialize dp array
-        for (int i = 0; i <= N; i++) {
-            dp[i][1] = 1; // Any number can be represented as the sum of exactly 1 integer in only one way
-        }
-        for (int i = 0; i <= K; i++) {
-            dp[0][i] = 1; // The number 0 can be represented in exactly 1 way
-        }
+        for (int i=0; i<=N; i++) 
+            dp[i][1] = 1;
 
-        // Fill the dp array
-        for (int i = 1; i <= N; i++) {
-            for (int j = 2; j <= K; j++) {
-                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD; // Apply MOD to the entire sum
+        for (int i=0; i<=K; i++)
+            dp[1][i] = i;
+
+        for (int i=2; i<=N; i++) {
+
+            for (int j=2; j<=K; j++) {
+
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD;
             }
         }
 
