62bdd08392a09725bcd796c669ce191c6f8e962a
Jinwoo Park <zvzv9808@gmail.com>
2024-09-21 22:38:33 +0900

    [Silver I] Title: 어두운 건 무서워, Time: 432 ms, Memory: 85560 KB -BaekjoonHub

---
+++ 백준/Silver/16507. 어두운 건 무서워/어두운 건 무서워.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int width, height, question, sum;
+        int r1, c1, r2, c2;
+        
+        //(1,1) ~ (x,y)까지의 합 저장
+        int[][] picture;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        width = Integer.parseInt(st.nextToken());
+        height = Integer.parseInt(st.nextToken());
+        question = Integer.parseInt(st.nextToken());
+
+        picture = new int[width + 1][height + 1];
+
+        for (int x = 1; x <= width; x++) {
+
+            st = new StringTokenizer(br.readLine());
+
+            // picture[x]의 합 저장
+            sum = 0;
+
+            for (int y = 1; y <= height; y++) {
+
+                sum += Integer.parseInt(st.nextToken());
+                picture[x][y] = sum + picture[x - 1][y];
+            }
+        }
+
+        for (int x = 0; x < question; x++) {
+            st = new StringTokenizer(br.readLine());
+
+            r1 = Integer.parseInt(st.nextToken());
+            c1 = Integer.parseInt(st.nextToken());
+            r2 = Integer.parseInt(st.nextToken());
+            c2 = Integer.parseInt(st.nextToken());
+
+            sb.append((picture[r2][c2] - picture[r1 - 1][c2] - picture[r2][c1 - 1] + picture[r1 - 1][c1 - 1]) / ((r2 - r1 + 1) * (c2 - c1 + 1))).append("\n");
+
+        }
+
+        System.out.print(sb);
+
+    }
+
+}
