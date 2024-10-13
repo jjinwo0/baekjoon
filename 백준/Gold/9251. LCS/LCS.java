14e54fc7311f1a08e014c63dc6031e205074b922
Jinwoo Park <zvzv9808@gmail.com>
2024-10-13 23:53:13 +0900

    [Gold V] Title: LCS, Time: 100 ms, Memory: 16080 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 입력부터 다 처리해놓고 고민할래~
+        String strSequence1 = br.readLine();
+        String strSequence2 = br.readLine();
+
+        int[][] dp = new int[strSequence1.length() + 1][strSequence2.length() + 1];
+
+        for (int i = 1; i <= strSequence1.length(); i ++) {
+            for (int j = 1; j <= strSequence2.length(); j ++) {
+                if (strSequence1.charAt(i - 1) == strSequence2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[strSequence1.length()][strSequence2.length()]);
+        br.close();
+    }
+}
